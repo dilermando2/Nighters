e6bfe149ec5f7a0c7dd47419bbe6d0042efa3b24
dilermando2 <ce_lo_loco@hotmail.com>
2017-08-30 19:57:15 -0300

    taqui

--- app/src/main/java/com/example/marc/nighters/MainActivity.java
+++ app/src/main/java/com/example/marc/nighters/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.marc.nighters;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
     }
 }
